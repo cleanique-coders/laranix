3b503bbea5077f9ca87c908e2b709ee7a7158aae
Cleanique Coders <cleanique.coders@gmail.com>
2016-05-14 07:05:33 +0800

    Ensure Laravel project owned by Apache default user

--- laranix.sh
+++ laranix.sh
@@ -9,11 +9,14 @@ public=$project"/public"
 domain=$1".dev"
 
 # create laravel project
-composer create-project --prefer-dist laravel/laravel $1
+sudo composer create-project --prefer-dist laravel/laravel $1
 echo "Project created at "$project
 
-chmod 777 $cache -R && chmod 777 $storage -R
+sudo chmod 777 $cache -R && chmod 777 $storage -R
 echo "Change $cache & $storage to writable"
+
+sudo chown www-data:www-data $project -R
+echo "Change owner to web server"
 
 virtualhost create $1.dev $public
 echo "Automate generate domain for Laravel project"
